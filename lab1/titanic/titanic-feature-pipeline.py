c54792309bcc066ea4eb9304fa187df7c7dee330
Abdullah1428 <abdullahcse1428@gmail.com>
2022-11-23 13:08:38 +0100

    added feature store to hopsworks for titanic dataset

--- lab1/titanic/titanic-feature-pipeline.py
+++ lab1/titanic/titanic-feature-pipeline.py
@@ -1,24 +1,32 @@
-import os
-import modal
-import great_expectations as ge
 import hopsworks
 import pandas as pd
 
 project = hopsworks.login()
 fs = project.get_feature_store()
 
-iris_df = pd.read_csv("https://repo.hops.works/master/hopsworks-tutorials/data/iris.csv")
+titanic_df = pd.read_csv("https://raw.githubusercontent.com/ID2223KTH/id2223kth.github.io/master/assignments/lab1"
+                         "/titanic.csv")
 
-iris_fg = fs.get_or_create_feature_group(
-    name="iris_modal",
+# Drop less important columns
+titanic_df.drop(['Name', 'Ticket', 'SibSp', 'Parch', 'Cabin', 'Embarked'], axis=1, inplace=True)
+
+# Handle missing values (age)
+titanic_df['Age'].fillna((titanic_df['Age'].mean()), inplace=True)
+
+# Map to numeric values (gender)
+categories = {"female": 1, "male": 0}
+titanic_df['Sex'] = titanic_df['Sex'].map(categories)
+
+titanic_fg = fs.get_or_create_feature_group(
+    name="titanic_modal",
     version=1,
-    primary_key=["sepal_length","sepal_width","petal_length","petal_width"], 
-    description="Iris flower dataset")
-iris_fg.insert(iris_df, write_options={"wait_for_job" : False})
+    primary_key=["PassengerId", "Pclass", "Sex", "Age", "Fare"],
+    description="Titanic dataset")
+titanic_fg.insert(titanic_df, write_options={"wait_for_job": False})
 
-expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")    
-value_between(expectation_suite, "sepal_length", 4.5, 8.0)
-value_between(expectation_suite, "sepal_width", 2.1, 4.5)
-value_between(expectation_suite, "petal_length", 1.2, 7)
-value_between(expectation_suite, "petal_width", 0.2, 2.5)
-iris_fg.save_expectation_suite(expectation_suite=expectation_suite, validation_ingestion_policy="STRICT")   
+# expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")
+# value_between(expectation_suite, "sepal_length", 4.5, 8.0)
+# value_between(expectation_suite, "sepal_width", 2.1, 4.5)
+# value_between(expectation_suite, "petal_length", 1.2, 7)
+# value_between(expectation_suite, "petal_width", 0.2, 2.5)
+# iris_fg.save_expectation_suite(expectation_suite=expectation_suite, validation_ingestion_policy="STRICT")
