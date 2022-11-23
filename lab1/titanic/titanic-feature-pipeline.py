d2cae94c4f0952ff511f24756df226458a120388
Abdullah1428 <abdullahcse1428@gmail.com>
2022-11-23 23:31:40 +0100

    training pipeline for titanic added

--- lab1/titanic/titanic-feature-pipeline.py
+++ lab1/titanic/titanic-feature-pipeline.py
@@ -20,16 +20,11 @@ titanic_df['Age'] = pd.cut(x=titanic_df['Age'], bins=bins, labels=False)
 categories = {"female": 1, "male": 0}
 titanic_df['Sex'] = titanic_df['Sex'].replace(categories)
 
+titanic_df['Fare'] = titanic_df['Fare'].fillna(-1)
+
 titanic_fg = fs.get_or_create_feature_group(
     name="titanic_modal",
     version=1,
     primary_key=["PassengerId"],
     description="Titanic dataset")
 titanic_fg.insert(titanic_df, write_options={"wait_for_job": False})
-
-# expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")
-# value_between(expectation_suite, "sepal_length", 4.5, 8.0)
-# value_between(expectation_suite, "sepal_width", 2.1, 4.5)
-# value_between(expectation_suite, "petal_length", 1.2, 7)
-# value_between(expectation_suite, "petal_width", 0.2, 2.5)
-# iris_fg.save_expectation_suite(expectation_suite=expectation_suite, validation_ingestion_policy="STRICT")
