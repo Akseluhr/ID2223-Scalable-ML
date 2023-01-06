6b4c3e7a304a83094b1b1dba7bb9d50aad17bd7d
Aksel Uhr <akseluhr8@gmail.com>
2023-01-06 15:22:06 +0100

    Upload the 50th most recent prices every 60th second works

--- project/feature-pipeline.py
+++ project/feature-pipeline.py
@@ -1,28 +1,68 @@
 import pandas as pd
 import requests
-import datetime
+from datetime import datetime
 from time import sleep, time
 from concurrent.futures import ThreadPoolExecutor
 import random
+import hopsworks
 
-
+project = hopsworks.login()
+fs = project.get_feature_store()
 
 
 # endpoint to get bitcoin price
 binanceAPI = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 FREQUNCY = 1
-data_dict = {"btc_price": 0,"event_time": datetime.datetime.now()}
+dt = datetime.now()
+ts = datetime.timestamp(dt)
+
+data_dict = {"btc_price": 0,"event_time": ts}
 blockchainAPI = "https://blockchain.info/q/hashrate"
+
+btc_price = []
+timestamp = []
+df_initial = pd.DataFrame(columns=['btc_price', 'timestamp'])
 
 
 def get_btc_price():
     while True:
         r = requests.get(binanceAPI)
-        data_dict['btc_price'] = float(r.json()['price'])
-        data_dict['event_time'] = datetime.datetime.now()
-        print(data_dict)
+        btc_price.append(float(r.json()['price']))
+        dt = datetime.now()
+        ts = datetime.timestamp(dt)
+        timestamp.append(ts)
+       # df = pd.DataFrame(data_dict)
+       # print(btc_price)
+       # print(timestamp)
+       # if (len(btc_price) >= 10):
+       #     sleep(10) # Upload every 10 seconds 
+       #     upload_to_hopsworks(btc_price, timestamp, True)
+       # else:
+       #     upload_to_hopsworks(btc_price, timestamp, False)
+       # print(timestamp)
+       # print(btc_price)
+
         sleep(1/FREQUNCY)
 
+def upload_to_hopsworks():
+    while True:
+          
+        btc_df = df_initial.append(pd.DataFrame(list(zip(btc_price, timestamp)), columns=['btc_price', 'timestamp']))
+        version = 1
+        # keeps the 50 most recent records
+        if len(btc_df) > 50:
+            btc_df = btc_df.drop(btc_df.index[0:len(btc_df)-50]).reset_index(drop=True)
+            version +=1
+            btc_fg = fs.get_or_create_feature_group(
+                name="btc_modal",
+                version=version,
+                primary_key=["btc_price", "timestamp"],
+                description="Btc dataset")
+            btc_fg.insert(btc_df, write_options={"wait_for_job": False})
+        print(btc_df)
+        
+        # Every 60 seconds we upload our new DF to hopsworks
+        sleep(60/FREQUNCY)
 '''
 def get_hash_rate():
 	while True:
@@ -33,8 +73,8 @@ def get_hash_rate():
 '''
 
 if __name__ == "__main__":
-	executor = ThreadPoolExecutor(3)
-	for func in [get_btc_price]:
+	executor = ThreadPoolExecutor(2)
+	for func in [get_btc_price, upload_to_hopsworks]:
 		executor.submit(func)
 
 	executor.shutdown(wait=True)
