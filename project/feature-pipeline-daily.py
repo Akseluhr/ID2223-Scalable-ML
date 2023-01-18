b9349172ff768d90bc3411c73cb476705192c941
Abdullah1428 <abdullahcse1428@gmail.com>
2023-01-18 15:50:45 +0100

    added feature pipeline daily

--- project/feature-pipeline-daily.py
+++ project/feature-pipeline-daily.py
@@ -1,30 +1,42 @@
+import datetime
+
 import modal
 
 LOCAL = False
 
+
 def feature_daily():
     import hopsworks
-    import datetime as dt
     from pandas_datareader import data as pdr
     import yfinance as yf
 
     # add the new data here.
+    today_date = datetime.datetime.today()
+
+    yf.pdr_override()
+    btc_df = pdr.get_data_yahoo('BTC-USD', start=today_date)
+    btc_df.drop(columns=['Adj Close'], inplace=True)
+
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+
+    fg = fs.get_feature_group(name="btc_modal_2", version=1)
+    fg.insert(btc_df, write_options={"wait_for_job": False})
+
 
 if not LOCAL:
     stub = modal.Stub()
-    image = modal.Image.debian_slim().apt_install(["libgomp1"]).pip_install([
-        "hopsworks==3.0.4", "seaborn", "joblib", "scikit-learn==1.0.2", "xgboost==1.5", "dataframe-image", "pandas",
-        "datetime", "requests", "python-dotenv", "yfinance", "pandas_datareader"])
-
+    image = modal.Image.debian_slim().pip_install(
+        ["hopsworks==3.0.4", "joblib", "seaborn", "scikit-learn", "xgboost", "dataframe-image", "datetime",
+         "yfinance", "pandas_datareader"])
 
     @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
     # @stub.function(image=image, secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
     def modal_btc_feature_daily():
         feature_daily()
 
-
 if __name__ == "__main__":
     if LOCAL:
         feature_daily()
     else:
-        stub.deploy("btc_modal_feature_daily")
+        stub.deploy("btc_modal_2_feature_daily")
